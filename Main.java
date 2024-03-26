02b5b7acc13ce498609be50a1aeb276abb3283c6
Karthi-018 <113188095+Karthi-018@users.noreply.github.com>
2024-03-26 21:14:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main
 {
     public static void main(String[] args) {
         System.out.println("Hi Hello From java");
-        Student s = new Student();
+        // Student s = new Student();
         
 
     }
